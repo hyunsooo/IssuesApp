9a578c9558dd87adbe7f9cc2d437438ea2e9cad0
hyunsoo <dreamh1122@gamil.com>
2017-10-28 16:53:41 +0900

    Initial Commit

---
+++ IssuesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IssuesApp
+//
+//  Created by hyunsu han on 2017. 10. 28..
+//  Copyright © 2017년 hyunsu han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
